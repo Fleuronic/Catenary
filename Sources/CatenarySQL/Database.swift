88efa2131c83cd89203b8d9d32ab04a991c10991
Jordan Kay <jordan@fleuronic.com>
2021-12-22 19:56:00 -0500

    Improve protocol

--- Sources/CatenarySQL/Database.swift
+++ Sources/CatenarySQL/Database.swift
@@ -1,7 +1,28 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
+import Schemata
 import PersistDB
 
 public protocol Database {
-	mutating func clear() async throws
+	var types: [AnyModel.Type] { get }
+	var storeKeyPath: WritableKeyPath<Self, Store<ReadWrite>> { get }
+}
+
+// MARK: -
+public extension Database {
+	func createStore() async throws -> Store<ReadWrite> {
+		try await .open(for: types)
+	}
+
+	mutating func clear() async throws {
+		try Store<ReadWrite>.destroy()
+		try await updateStore()
+	}
+}
+
+// MARK: -
+private extension Database {
+	mutating func updateStore() async throws {
+		self[keyPath: storeKeyPath] = try await createStore()
+	}
 }
