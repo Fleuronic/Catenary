5ced87f764419088d778c071981b3e90b97e24d5
Jordan Kay <jordan@fleuronic.com>
2022-01-04 23:42:15 -0500

    Remove extensions

--- Sources/CatenarySQL/Model.swift
+++ Sources/CatenarySQL/Model.swift
@@ -4,18 +4,7 @@ import PersistDB
 
 public protocol Model: PersistDB.Model, PersistDB.ModelProjection {
 	var valueSet: ValueSet<Model> { get }
-}
+	var identifiedValueSet: ValueSet<Model> { get }
 
-// MARK: -
-extension Model {
-	var identifiedValueSet: ValueSet<Model> {
-		valueSet.update(with: [\.id == id])
-	}
-}
-
-// MARK: -
-public extension Model {
-	static var defaultOrder: [Ordering<Self>] {
-		[.init(\Self.id, ascending: true)]
-	}
+	static var defaultOrder: [Ordering<Self>] { get }
 }
