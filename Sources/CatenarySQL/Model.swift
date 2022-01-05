155c3d7202a24009b6d4b8b374e396168011139e
Jordan Kay <jordan@fleuronic.com>
2022-01-04 23:48:48 -0500

    Add id keypath and property

--- Sources/CatenarySQL/Model.swift
+++ Sources/CatenarySQL/Model.swift
@@ -1,10 +1,30 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
+import Schemata
 import PersistDB
 
 public protocol Model: PersistDB.Model, PersistDB.ModelProjection {
 	var valueSet: ValueSet<Model> { get }
-	var identifiedValueSet: ValueSet<Model> { get }
+}
 
-	static var defaultOrder: [Ordering<Self>] { get }
+// MARK: -
+extension Model {
+	var identifiedValueSet: ValueSet<Model> {
+		valueSet.update(with: [\.id == id])
+	}
+}
+
+// MARK: -
+public extension Model {
+	static var idKeyPath: KeyPath<Self, ID> {
+		\.id
+	}
+
+	static var idProperty: Property<Self, ID> {
+		\.id ~ "id"
+	}
+
+	static var defaultOrder: [Ordering<Self>] {
+		[.init(\Self.id, ascending: true)]
+	}
 }
