ff0c78bc1d013d407bb894ad436ef8385988b304
Jordan Kay <jordan@fleuronic.com>
2022-01-05 01:19:01 -0500

    Remove unused extension

--- Sources/CatenarySQL/Valued.swift
+++ Sources/CatenarySQL/Valued.swift
@@ -1,13 +1,7 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
-import Schemata
 import PersistDB
 
 public protocol Valued: PersistDB.ModelProjection {
 	var valueSet: ValueSet<Model> { get }
-}
-
-// MARK: -
-public extension Valued {
-	var valueSet: ValueSet<Model> { .init() }
 }
