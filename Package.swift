cff033ed7e53fcc584cc689e87ea4c19be38a2ce
Jordan Kay <jordan@fleuronic.com>
2022-04-01 19:11:29 -0400

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -29,6 +29,7 @@ let package = Package(
 		.package(url: "https://github.com/vapor/postgres-kit", .upToNextMajor(from: "2.5.0")),
 		.package(url: "https://github.com/Fleuronic/PersistDB", .branch("master")),
 		.package(url: "https://github.com/JohnSundell/Identity", from: "0.1.0"),
+		.package(url: "https://github.com/Fleuronic/AsyncExtensions", .upToNextMajor(from: "0.4.0")),
 		.package(url: "https://github.com/simba909/ReactiveCombineBridge", .branch("master"))
 	],
 	targets: [
@@ -52,6 +53,7 @@ let package = Package(
 				"Catenary",
 				"PersistDB",
 				"Identity",
+				"AsyncExtensions",
 				"ReactiveCombineBridge"
 			]
 		),
--- Sources/CatenarySQL/Store/Store.swift
+++ Sources/CatenarySQL/Store/Store.swift
@@ -2,6 +2,7 @@
 
 import Foundation
 import Combine
+import AsyncExtensions
 import ReactiveCombineBridge
 import Schemata
 import PersistDB
