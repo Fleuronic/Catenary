3d8d0604515b65b149ebd07ed8bc256fb1794ca4
Jordan Kay <jordan@fleuronic.com>
2023-02-01 23:27:11 -0500

    Support POST in REST API

--- Sources/Catenary/API/Architectures/REST/RESTAPI.swift
+++ Sources/Catenary/API/Architectures/REST/RESTAPI.swift
@@ -11,20 +11,60 @@ public protocol RESTAPI: API {}
 // MARK: -
 public extension RESTAPI {
 	func getResource<Resource: Decodable>(at path: String) async -> Result<Resource> {
-		await resource(path: path, method: "GET")
+		await resource(
+			path: path,
+			method: "GET"
+		)
 	}
 
-	func resource<Resource: Decodable>(path: String, method: String) async -> Result<Resource> {
+	func post<Resource: Decodable>(_ data: Data, to path: String) async -> Result<Resource> {
+		await resource(
+			path: path,
+			method: "POST",
+			body: data
+		)
+	}
+
+	func postResource<Resource: Decodable, PostedResource: Encodable>(_ postedResource: PostedResource, to path: String) async -> Result<Resource> {
+		await resource(
+			path: path,
+			method: "POST",
+			body: try! encoder.encode(postedResource)
+		)
+	}
+}
+
+// MARK: -
+extension RESTAPI {
+	func resource<Resource: Decodable>(from data: Data) throws -> Resource {
+		do {
+			return try decoder.decode(Response.self, from: data).resource()
+		} catch is DecodingError {
+			throw try decoder.decode(Error.self, from: data)
+		}
+	}
+}
+
+// MARK: -
+private extension RESTAPI {
+	func resource<Resource: Decodable>(
+		path: String,
+		method: String,
+		body: Data? = nil
+	) async -> Result<Resource> {
 		do {
 			let url = URL(string: "\(baseURL.absoluteString)\(path)")!
 			var urlRequest = URLRequest(url: url)
 			urlRequest.httpMethod = method
+			urlRequest.httpBody = body
+			print(String(decoding: body!, as: UTF8.self))
 
 			if let resource: Result<Resource> = try await mockResource(path: path, method: method) {
 				return resource
 			}
 
 			let (data, _) = try await URLSession.shared.data(for: urlRequest)
+			print(String(decoding: data, as: UTF8.self))
 			return try .success(resource(from: data))
 		} catch let error as Error {
 			return .failure(.api(error))
@@ -32,14 +72,6 @@ public extension RESTAPI {
 			return .failure(.decoding(error))
 		} catch let error as NSError {
 			return .failure(.network(error))
-		}
-	}
-
-	func resource<Resource: Decodable>(from data: Data) throws -> Resource {
-		do {
-			return try decoder.decode(Response.self, from: data).resource()
-		} catch is DecodingError {
-			throw try decoder.decode(Error.self, from: data)
 		}
 	}
 }
