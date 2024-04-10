81282fc2304027d7b1baf4ccb7a3759306c1f667
Jordan Kay <jordan@fleuronic.com>
2024-04-10 18:23:20 -0400

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/Fleuronic/Catena", branch: "main"),
+		.package(url: "https://github.com/almazrafi/DictionaryCoder.git", from: "1.2.0"),
 		.package(url: "https://github.com/NicholasBellucci/SociableWeaver.git", from: "0.1.0")
 	],
 	targets: [
@@ -26,6 +27,7 @@ let package = Package(
 			name: "Catenary",
 			dependencies: [
 				"Catena",
+				"DictionaryCoder",
 				"SociableWeaver"
 			]
 		)
