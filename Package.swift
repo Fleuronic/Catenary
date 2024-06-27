446099df021137202b22e792e2c3416adaeebedd
Jordan Kay <jordan@fleuronic.com>
2024-06-27 10:10:23 -0700

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.10
 // Copyright © Fleuronic LLC. All rights reserved.
 
 import PackageDescription
@@ -18,7 +18,7 @@ let package = Package(
 		)
 	],
 	dependencies: [
-		.package(path: "../..")
+        .package(url: "https://github.com/Fleuronic/Catena", branch: "main")
 	],
 	targets: [
 		.target(
